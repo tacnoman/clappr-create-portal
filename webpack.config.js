2ef22c09ad3a48b0fc00c40233c9eb47552e7017
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2018-01-19 12:39:14 -0200

    add gitignore
    remove node modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'clappr-create-portal.js'
+    filename: 'clappr-create-portal.js',
+    library: 'ClapprCreatePortal',
   },
   module: {
   loaders: [
